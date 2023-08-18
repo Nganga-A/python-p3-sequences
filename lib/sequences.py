b41dd0ddc52962b7a656b422d63bff436a0972a1
Abed Nganga <ngangaabed@gmail.com>
2023-08-18 23:25:35 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers
+
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    print(fib_sequence)
+
+# Example usage:
+sequence_length = int(input("Enter the length of Fibonacci sequence: "))
+print_fibonacci(sequence_length)
